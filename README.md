2429aa3935e30fe897acda3f4d31478af88b7c4b
celine-g <54836830+celine-g@users.noreply.github.com>
2019-11-05 22:21:47 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # inLine
+
+Erstelldatum: November 2019
+Entwickler: Celine Götz und Inka Ernst
+
+Dieses Projekt beinhaltet eine Website, um ausstehende Einkäufe in Listen zu organisieren. 
+Folgende 6 Listen sind vordefiniert: 
+1. Supermarkt
+2. Drogerie
+3. Bäckerei
+4. Apotheke
+5. Baumarkt
+6. Sonstiges
+
+Jeder Liste kann bis zu 15 Items enthalten. Mittels des Eingabefeldes auf der rechten Seite können beliebige Items hinzugefügt werden. 
+Durch das Llicken des Kreuzes rechts neben des jeweiligen Items, kann dieses aus der Liste entfernt werden. 
+Durch Klicken auf das Item, kann es als "gekauft" markiert werden. Das Feld färbt sich dann grün. 
+Wird erneut auf das Feld geklickt, kann man diesen Vorgang rückgängig machen. 
+
+Neue Listen können nicht angelegt werden.
+
+Das Design wurde schlicht gehalten, sodass die Bedienung einfach und selbsterklärend ist. 
+
+Die Website ist auf allen Displaygrößen formatiert, d.h. sie kann z.B. gleichzeitig auf dem Laptop und Smartphone genutzt werden. 
+
+Viel Spaß mit InLine ! Über Feedback und Verbesserungsvorschläge freuen wir uns jederzeit.
+
+Inka und Celine 
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const baseUrl = "https://shopping-lists-api.herokuapp.com/api/v1/lists/";
 var listId;
+var aktuellesJson;
 
-//Funktion um bei Klick auf eine Liste, diese im Feld rechts anzeigen zu lassen
+
 function getList(uebergebene_listId) {
 
     var url = baseUrl + uebergebene_listId;
@@ -29,41 +30,185 @@ function getList(uebergebene_listId) {
             for (let i = 0; i < jsonObjekt.items.length; i++) {
                 var htmlId = "item" + i;
                 if (jsonObjekt.items[i].name != null) {
-                    document.getElementById(htmlId).innerHTML = 
-                    '<form class="form"><div class="inputGroup"><input type="checkbox" id="option1"><label for="option1" onclick="itemAbhaken(' + "'" + jsonObjekt.items[i]._id + "'" + ')></label></div></form>'
-                   + jsonObjekt.items[i].name + '<button onclick="itemLoeschen(' + "'" + jsonObjekt.items[i]._id + "'" + ')">löschen</button>';
+                    //Wenn Itemstatus = true wird item mit checked erzeugt, bei Itemstatus= false mit unchecked
+                    if (jsonObjekt.items[i].bought == true) {
+                        document.getElementById(htmlId).innerHTML =
+                            '<div id="checked" class="listenelement" onclick="itemAbhaken(' + "'" + jsonObjekt.items[i]._id + "'" + ')" >'
+                            + '<input type="checkbox" checked >'
+                            + '<p id="listenelement_content">' + jsonObjekt.items[i].name + '</p>'
+                            + '<button id="loeschButton" title="Item löschen" onclick="itemLoeschen(' + "'" + jsonObjekt.items[i]._id + "'" + ')"> x </button>';
+                        + '</div>'
+                    }
+                    else {
+                        document.getElementById(htmlId).innerHTML =
+                            '<div id="unchecked" class="listenelement" onclick="itemAbhaken(' + "'" + jsonObjekt.items[i]._id + "'" + ')" >'
+                            + '<input type="checkbox">'
+                            + '<p id="listenelement_content">' + jsonObjekt.items[i].name + '</p>'
+                            + '<button id="loeschButton" title="Item löschen" onclick="itemLoeschen(' + "'" + jsonObjekt.items[i]._id + "'" + ')"> x </button>';
+                        + '</div>'
+                    }
                 }
             }
+            //Listenname (Überschrift) setzen und Wilkommenstext zurücksetzen
+            aktuellesJson = jsonObjekt;
             document.getElementById("listName").innerHTML = jsonObjekt.name;
             document.getElementById("standardText").innerHTML = "";
 
-            /*Sobald eine Liste ausgewählt wurde, wird Eingabefeld für neues Item 
-            angezeigt, indem style="hidden" aufgehoben wird*/
-            for (var i = 0, h1 = document.getElementsByTagName("h1"); i < h1.length; i++) {
-                h1[i].style.color = "red";
-            }
+            //Auf Startseite ist Itemeingabefeld verborgen, beim Aufrufen der getList-Funktion, wird dieses wieder angezeigt
+            var x = document.getElementById("itemHinzufuegen");
+            x.style.display = "block";
 
-            //Variable listId setzen, damit ItemsHinzufuegen()-Funktion damit "arbeiten" kann
+            //Variable listId global setzen, damit ItemsHinzufuegen()-Funktion damit "arbeiten" kann
             listId = jsonObjekt._id;
+
+            //Hintergrundbilder dynamisch einfügen
+            document.getElementById("main").style['backgroundImage'] = "url(bilder/" + listId + ".jpg)";
+
+            //Style der aktiven Liste - Leider sehr redundant :/ 
+           switch (listId) {
+                case "5da717aa7736ad00170dd8f7":
+                    var a = document.getElementById("5da717aa7736ad00170dd8f7");
+                    a.style.color = "#c06c84";
+                    a.style.transform = "scale(1.2)";
+                    var b = document.getElementById("5da717b87736ad00170dd8f9");
+                    var c = document.getElementById("5da717bf7736ad00170dd8fa");
+                    var d = document.getElementById("5da717af7736ad00170dd8f8");
+                    var e = document.getElementById("5da717c57736ad00170dd8fb");
+                    var f = document.getElementById("5da717de7736ad00170dd8fc");
+                    b.style.color = "#fff";
+                    b.style.transform = "scale(1)";
+                    c.style.color = "#fff";
+                    c.style.transform = "scale(1)";
+                    d.style.color = "#fff";
+                    d.style.transform = "scale(1)";
+                    e.style.color = "#fff";
+                    e.style.transform = "scale(1)";
+                    f.style.color = "#fff";
+                    f.style.transform = "scale(1)";
+                    break;
+                case "5da717b87736ad00170dd8f9":
+                    var a = document.getElementById("5da717b87736ad00170dd8f9");
+                    a.style.color = "#c06c84";
+                    a.style.transform = "scale(1.2)";
+                    var b = document.getElementById("5da717aa7736ad00170dd8f7");
+                    var c = document.getElementById("5da717bf7736ad00170dd8fa");
+                    var d = document.getElementById("5da717af7736ad00170dd8f8");
+                    var e = document.getElementById("5da717c57736ad00170dd8fb");
+                    var f = document.getElementById("5da717de7736ad00170dd8fc");
+                    b.style.color = "#fff";
+                    b.style.transform = "scale(1)";
+                    c.style.color = "#fff";
+                    c.style.transform = "scale(1)";
+                    d.style.color = "#fff";
+                    d.style.transform = "scale(1)";
+                    e.style.color = "#fff";
+                    e.style.transform = "scale(1)";
+                    f.style.color = "#fff";
+                    f.style.transform = "scale(1)";
+                    break;
+                case "5da717bf7736ad00170dd8fa":
+                    var a = document.getElementById("5da717bf7736ad00170dd8fa");
+                    a.style.color = "#c06c84";
+                    a.style.transform = "scale(1.2)";
+                    var b = document.getElementById("5da717aa7736ad00170dd8f7");
+                    var c = document.getElementById("5da717b87736ad00170dd8f9");
+                    var d = document.getElementById("5da717af7736ad00170dd8f8");
+                    var e = document.getElementById("5da717c57736ad00170dd8fb");
+                    var f = document.getElementById("5da717de7736ad00170dd8fc");
+                    b.style.color = "#fff";
+                    b.style.transform = "scale(1)";
+                    c.style.color = "#fff";
+                    c.style.transform = "scale(1)";
+                    d.style.color = "#fff";
+                    d.style.transform = "scale(1)";
+                    e.style.color = "#fff";
+                    e.style.transform = "scale(1)";
+                    f.style.color = "#fff";
+                    f.style.transform = "scale(1)";
+                    break;
+                case "5da717af7736ad00170dd8f8":
+                    var a = document.getElementById("5da717af7736ad00170dd8f8");
+                    a.style.color = "#c06c84";
+                    a.style.transform = "scale(1.2)";
+                    var b = document.getElementById("5da717aa7736ad00170dd8f7");
+                    var c = document.getElementById("5da717b87736ad00170dd8f9");
+                    var d = document.getElementById("5da717bf7736ad00170dd8fa");
+                    var e = document.getElementById("5da717c57736ad00170dd8fb");
+                    var f = document.getElementById("5da717de7736ad00170dd8fc");
+                    b.style.color = "#fff";
+                    b.style.transform = "scale(1)";
+                    c.style.color = "#fff";
+                    c.style.transform = "scale(1)";
+                    d.style.color = "#fff";
+                    d.style.transform = "scale(1)";
+                    e.style.color = "#fff";
+                    e.style.transform = "scale(1)";
+                    f.style.color = "#fff";
+                    f.style.transform = "scale(1)";
+                    break;
+                case "5da717c57736ad00170dd8fb":
+                    var a = document.getElementById("5da717c57736ad00170dd8fb");
+                    a.style.color = "#c06c84";
+                    a.style.transform = "scale(1.2)";
+                    var b = document.getElementById("5da717aa7736ad00170dd8f7");
+                    var c = document.getElementById("5da717b87736ad00170dd8f9");
+                    var d = document.getElementById("5da717bf7736ad00170dd8fa");
+                    var e = document.getElementById("5da717af7736ad00170dd8f8");
+                    var f = document.getElementById("5da717de7736ad00170dd8fc");
+                    b.style.color = "#fff";
+                    b.style.transform = "scale(1)";
+                    c.style.color = "#fff";
+                    c.style.transform = "scale(1)";
+                    d.style.color = "#fff";
+                    d.style.transform = "scale(1)";
+                    e.style.color = "#fff";
+                    e.style.transform = "scale(1)";
+                    f.style.color = "#fff";
+                    f.style.transform = "scale(1)";
+                    break;
+                case "5da717de7736ad00170dd8fc":
+                    var a = document.getElementById("5da717de7736ad00170dd8fc");
+                    a.style.color = "#c06c84";
+                    a.style.transform = "scale(1.2)";
+                    var b = document.getElementById("5da717aa7736ad00170dd8f7");
+                    var c = document.getElementById("5da717b87736ad00170dd8f9");
+                    var d = document.getElementById("5da717bf7736ad00170dd8fa");
+                    var e = document.getElementById("5da717af7736ad00170dd8f8");
+                    var f = document.getElementById("5da717c57736ad00170dd8fb");
+                    b.style.color = "#fff";
+                    b.style.transform = "scale(1)";
+                    c.style.color = "#fff";
+                    c.style.transform = "scale(1)";
+                    d.style.color = "#fff";
+                    d.style.transform = "scale(1)";
+                    e.style.color = "#fff";
+                    e.style.transform = "scale(1)";
+                    f.style.color = "#fff";
+                    f.style.transform = "scale(1)";
+                    break;
+            }
         }
+
     }
 
     request.send();
 
 }
 
-//Funktion, um Items der ausgewählen Liste hinzuzufügen
+
 function itemHinzufuegen() {
 
     var url = baseUrl + listId + "/items";
     var request = new XMLHttpRequest();
 
-    //Input-Feld aulesen und als der Variable name zuweisen
+    //Input-Feld auslesen und der Variable als name zuweisen
     var itemName = document.getElementById("itemHinzufuegen_input").value.toString();
     var item = {
         "name": itemName
     }
 
+    if (aktuellesJson.items.length <= 14) { 
+    
     request.open("POST", url, true);
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Authorization", "b96349221079c4008d434972f7b77efd");
@@ -76,15 +221,21 @@ function itemHinzufuegen() {
     }
 
     request.send(JSON.stringify(item));
+    }
+
+    else {
+        alert ("Mehr als 15 Einkäufe kannst du sowieso nicht tragen - Leider können keine weiteren Listenelemente hinzugefügt werden!");
+    }
 }
 
 
-//Funktion, um ein bestimmtes Item einer Liste zu löschen
+
 function itemLoeschen(itemId) {
 
     var url = baseUrl + listId + "/items/" + itemId;
     var request = new XMLHttpRequest();
 
+    //Http_Anfrage
     request.open("DELETE", url, true);
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Authorization", "b96349221079c4008d434972f7b77efd");
@@ -98,27 +249,43 @@ function itemLoeschen(itemId) {
 
 }
 
-//Funktion, um Status des Items zu setzen (Abhaken)
-function itemAbhaken(itemId) {
 
-    var url = baseUrl + listId + "/items/" + itemId;
+function itemAbhaken(uebergebene_itemId) {
+    console.log(aktuellesJson);
+    var url = baseUrl + listId.toString() + "/items/" + uebergebene_itemId.toString();
+    var arraystelle;
+    //Herausfinden, welche Stelle im Array die Id hat
+    for (let i = 0; i < aktuellesJson.items.length; i++) {
+        if (aktuellesJson.items[i]._id == uebergebene_itemId) {
+            arraystelle = i;
+        }
+    }
+ 
+    //Abfragen wie der aktuelle Status von bought ist und dementsprechend umsetzen
+    if (aktuellesJson.items[arraystelle].bought == true) {
+        status = false;
+    }
+    else {
+        status = true;
+    }
+
+    var itemStatus = {
+        "bought": status
+    }
+
     var request = new XMLHttpRequest();
-    var item = {
-        "bought": true
-    }
 
+    //Http-Anfrage
     request.open("PUT", url, true);
-    request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Authorization", "b96349221079c4008d434972f7b77efd");
+    request.setRequestHeader("Content-Type", "application/json");
     request.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var jsonObjekt = JSON.parse(this.responseText);
-            
+        if (request.readyState == 4 && request.status == 200) {
+            console.log(request.responseText);
             getList(listId);
-            console.log(getList('5da717aa7736ad00170dd8f7'));
-    }
+        }
+    };
 
-    request.send();
+    request.send(JSON.stringify(itemStatus));
 
-}
 }
